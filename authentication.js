3b244b386e0564ae0fdb31d8e7ca4e537cd98db9
kiran-lns <kiran@lockandstock.app>
2021-11-18 17:46:02 +0530

    first commit

---
+++ authentication.js
@@ -0,0 +1,29 @@
+/* eslint-disable arrow-body-style */
+const db = require("../models");
+const logger = require("../logger");
+const { getMessage } = require("../utils/messages");
+const { getDecodedToken } = require("../utils/token");
+
+exports.userAuthorization = () => {
+  // eslint-disable-next-line func-names
+  return async function (req, res, next) {
+    const { user: UserModel } = db;
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).send(getMessage("UNAUTHORIZED"));
+    }
+
+    try {
+      const decodedToken = await getDecodedToken(token);
+      const user = await UserModel.findByPk(decodedToken.sub);
+      req.user = user;
+      return next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res.status(405).send(getMessage("SESSION_EXPIRED"));
+      }
+      logger.info(error);
+      return next(error);
+    }
+  };
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+const { userAuthorization } = require("./authentication");
+
+module.exports = {
+  userAuthorization,
+};
